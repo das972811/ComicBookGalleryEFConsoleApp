b6d91c21e48c742756a15e381dbefb8f43039785
Diego Polar <Diego.Polar@baldwincountyal.gov>
2024-01-08 23:43:53 -0600

    Utilized dotnet ef to create migrations and update database by using sqllocaldb connection string; Created a new row in ComicBooks table

--- Context.cs
+++ Context.cs
@@ -1,10 +1,17 @@
-
-using ComicBookGallery.Models;
 using Microsoft.EntityFrameworkCore;
+using ComicBookGallery.Models;
+using System.Runtime.InteropServices;
 
 namespace ComicBookGallery;
 
 public class Context : DbContext
 {
     public DbSet<ComicBook> ComicBooks { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        string connectionString = "Server=(localdb)\\mylocaldb;Database=ComicBookGallery;Trusted_Connection=True;";
+
+        optionsBuilder.UseSqlServer(connectionString);
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ComicBookGallery;
+using ComicBookGallery.Models;
+
+using (var context = new Context())
+{
+    context.ComicBooks.Add(new ComicBook {
+        SeriesTitle = "The Amazing Spider-Man",
+        IssueNumber = 1,
+        Description = "Awesome comic book",
+        PublishedOn = DateTime.Now
+    });
+
+    context.SaveChanges();
+
+    var comicBooks = context.ComicBooks.ToList();
+    foreach (var comicBook in comicBooks)
+    {
+        Console.WriteLine(comicBook.SeriesTitle);
+    }
+
+    Console.ReadLine();
+}
