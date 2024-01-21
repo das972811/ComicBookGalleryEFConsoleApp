4ab44293d65864bb85fb62d09185bb1f68f27871
Diego Polar <Diego.Polar@baldwincountyal.gov>
2024-01-20 20:23:09 -0600

    Added cascade delete using the fluent api

--- Context.cs
+++ Context.cs
@@ -11,7 +11,7 @@ public class Context : DbContext
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         string connectionString = "Server=(localdb)\\mylocaldb;Database=ComicBookGallery;Trusted_Connection=True;";
-        
+
         optionsBuilder.UseSqlServer(connectionString);
         // optionsBuilder.LogTo(Console.WriteLine);
     }
@@ -30,6 +30,18 @@ public class Context : DbContext
         //     .WithMany(cb => cb.ComicBooks)
         //     .HasForeignKey(cb => cb.SeriesId)
         //     .IsRequired();
+        // modelBuilder.Entity<ComicBookArtist>()
+        //     .HasRequired(cba => cba.ComicBook)
+        //     .WithMany(cb => cb.Artists)
+        //     .HasForeignKey(cba => cba.ComicBookId)
+        //     .WillCascadeOnDelete(false);
+
+        modelBuilder.Entity<ComicBookArtist>()
+            .HasOne(cba => cba.ComicBook)
+            .WithMany(cba => cba.Artists)
+            .HasForeignKey(cba => cba.ComicBookId)
+            .OnDelete(DeleteBehavior.ClientCascade);
+
         modelBuilder.Entity<ComicBook>()
             .Property(cb => cb.AverageRating)
             .HasPrecision(5, 2);
